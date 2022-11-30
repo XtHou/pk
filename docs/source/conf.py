eee686f4ec3bac12ae8935dc22b4ed889ae8b4ec
XtHou <47288028+XtHou@users.noreply.github.com>
2022-11-30 14:20:05 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Porkey'
+copyright = '2022, AELF'
+author = 'AELF'
 
 release = '0.1'
 version = '0.1.0'
